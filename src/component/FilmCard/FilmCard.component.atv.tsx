1ce8bc5af746a756b6b176add7fa70d7e8491b67
falcofemoralis <vladyslav.ivashchenko0@gmail.com>
2025-03-12 09:50:06 +0200

    Adjust film card poster

--- src/component/FilmCard/FilmCard.component.atv.tsx
+++ src/component/FilmCard/FilmCard.component.atv.tsx
@@ -96,7 +96,10 @@ export function FilmCardComponent({
   );
 
   const renderPoster = () => (
-    <View style={ styles.posterWrapper }>
+    <View
+      style={ styles.posterWrapper }
+      renderToHardwareTextureAndroid
+    >
       <ThemedImage
         style={ [
           styles.poster,
